cd0fc8e0921471e0c94a2acd6d487fb652454677
Henning P. Schmiedehausen <henning@schmiedehausen.org>
2025-01-27 22:46:12 -0800

    wrap up migration work

--- scripts/migration.lua
+++ scripts/migration.lua
@@ -8,6 +8,7 @@ local util = require('util')
 
 local Position = require('stdlib.area.position')
 local Is = require('stdlib.utils.is')
+local table = require('stdlib.utils.table')
 
 local const = require('lib.constants')
 
@@ -78,33 +79,6 @@ local function create_config_from_staco(staco)
     return config
 end
 
----@param blueprint_entity BlueprintEntity
----@return Tags?
-local function create_tags(blueprint_entity)
-    local config = This.StackCombinator:createConfig()
-    local control = blueprint_entity.control_behavior
-    assert(control)
-
-    local parameters = control.arithmetic_conditions
-    assert(parameters)
-
-    local invert = parameters.first_signal and staco_signal_map[parameters.first_signal.name] or false
-    if invert then
-        config.network_settings[defines.wire_connector_id.circuit_red].invert = invert[1]
-        config.network_settings[defines.wire_connector_id.circuit_green].invert = invert[2]
-    end
-
-    config.merge_inputs = bit32.band(parameters.second_constant, 1) == 1
-    config.use_wagon_stacks = bit32.band(parameters.second_constant, 2) == 2
-    config.process_fluids = true
-
-    config.op = staco_op_map[parameters.operation]
-
-    return {
-        ['const.config_tag_name'] = config
-    }
-end
-
 ---@param surface LuaSurface
 ---@param staco LuaEntity
 local function migrate_staco(surface, staco)
@@ -166,48 +140,175 @@ function Migration:migrateStacos()
     end
 end
 
----@param blueprint LuaRecord
-local function migrate_blueprint(blueprint)
-    if blueprint.type == 'blueprint-book' then
-        for _, nested_blueprint in pairs(blueprint.contents) do
-            migrate_blueprint(nested_blueprint)
-        end
-        return
+---------------------------------------------------------------------------
+
+local control_behavior_template = {
+    arithmetic_conditions = {
+        second_constant = 0,
+        operation = '*',
+    }
+}
+
+---@param blueprint_entity BlueprintEntity
+---@return Tags?
+local function create_tags(blueprint_entity)
+    local config = This.StackCombinator:createConfig()
+    local control = blueprint_entity.control_behavior
+    assert(control)
+
+    local parameters = control.arithmetic_conditions
+    assert(parameters)
+
+    local invert = parameters.first_signal and staco_signal_map[parameters.first_signal.name] or false
+    if invert then
+        config.network_settings[defines.wire_connector_id.circuit_red].invert = invert[1]
+        config.network_settings[defines.wire_connector_id.circuit_green].invert = invert[2]
     end
 
-    if blueprint.type ~= 'blueprint' then return end
+    config.merge_inputs = bit32.band(parameters.second_constant, 1) == 1
+    config.use_wagon_stacks = bit32.band(parameters.second_constant, 2) == 2
+    config.process_fluids = true
 
-    for _, default_icon in pairs(blueprint.default_icons) do
-        if (default_icon.signal.type == nil or default_icon.signal.type == 'item') and const.migration.migrations[default_icon.signal.name] then
-            default_icon.signal.name = const.migration.migrations[default_icon.signal.name]
-        end
-    end
+    config.op = staco_op_map[parameters.operation]
 
+    return {
+        ['const.config_tag_name'] = config
+    }
+end
+
+--- Replace old staco with new Stack Combinator Redux
+---@param blueprint_entity BlueprintEntity
+---@return BlueprintEntity?
+local function create_entity(blueprint_entity)
+    local new_entity = util.copy(blueprint_entity)
+    new_entity.control_behavior = util.copy(control_behavior_template)
+    new_entity.name = const.migration.migrations[blueprint_entity.name]
+    new_entity.tags = create_tags(blueprint_entity)
+    return new_entity
+end
+
+---------------------------------------------------------------------------
+
+
+local BlueprintMigrator = {
+    is_cc_processor = table.array_to_dictionary {
+        'compaktcircuit-processor',
+        'compaktcircuit-processor_1x1',
+        'compaktcircuit-processor_with_tags',
+        'compaktcircuit_1x1',
+    }
+}
+
+---------------------------------------------------------------------------
+
+---@param blueprint_entity BlueprintEntity
+---@return BlueprintEntity?
+function BlueprintMigrator:migrateBlueprintEntity(blueprint_entity)
+    if not const.migration.migrations[blueprint_entity.name] then return nil end
+
+    return create_entity(blueprint_entity)
+end
+
+---@param processor_entity BlueprintEntity
+---@return boolean changed
+function BlueprintMigrator:processCompaktCircuitProcessor(processor_entity)
     local dirty = false
+    local inventory = game.create_inventory(1)
+    local item_stack = inventory[1]
 
-    local blueprint_entities = blueprint.get_blueprint_entities()
-    if not blueprint_entities then return end
+    if not processor_entity.tags.blueprint then return false end
 
-    for i = 1, blueprint.get_blueprint_entity_count() do
-        local blueprint_entity = blueprint_entities[i]
-
-        if const.migration.migrations[blueprint_entity.name] then
-            local new_entity = util.copy(blueprint_entity)
-            new_entity.name = const.migration.migrations[blueprint_entity.name]
-            new_entity.tags = create_tags(blueprint_entity)
-            blueprint_entities[i] = new_entity
+    if item_stack.import_stack(processor_entity.tags.blueprint) == 0 and item_stack.is_blueprint then
+        if self:executeMigration(item_stack) then
+            local tags = util.copy(processor_entity.tags)
+            tags.blueprint = item_stack.export_stack()
+            processor_entity.tags = tags
             dirty = true
         end
     end
 
-    if dirty then
-        blueprint.set_blueprint_entities(blueprint_entities)
+    inventory.destroy()
+    return dirty
+end
+
+---@param blueprint_entities (BlueprintEntity[])?
+---@return boolean modified
+function BlueprintMigrator:migrateBlueprintEntities(blueprint_entities)
+    local dirty = false
+
+    if not blueprint_entities then return dirty end
+
+    for i = 1, #blueprint_entities, 1 do
+        local blueprint_entity = blueprint_entities[i]
+
+        if self.is_cc_processor[blueprint_entity.name] then
+            dirty = self:processCompaktCircuitProcessor(blueprint_entity) or dirty
+        elseif const.migration.migrations[blueprint_entity.name] then
+            local new_entity = self:migrateBlueprintEntity(blueprint_entity)
+            if new_entity then
+                blueprint_entities[i] = new_entity
+                dirty = true
+            end
+        end
+    end
+
+    return dirty
+end
+
+---@param migration_object (LuaItemStack|LuaRecord)?
+---@return boolean
+function BlueprintMigrator:executeMigration(migration_object)
+    if not (migration_object and migration_object.valid) then return false end
+
+    local blueprint_entities = util.copy(migration_object.get_blueprint_entities())
+    if (self:migrateBlueprintEntities(blueprint_entities)) then
+        migration_object.set_blueprint_entities(blueprint_entities)
+        return true
+    end
+
+    return false
+end
+
+---@param inventory LuaInventory?
+function BlueprintMigrator:processInventory(inventory)
+    if not (inventory and inventory.valid) then return end
+    for i = 1, #inventory, 1 do
+        if inventory[i] then
+            if inventory[i].is_blueprint then
+                self:executeMigration(inventory[i])
+            elseif inventory[i].is_blueprint_book then
+                local nested_inventory = inventory[i].get_inventory(defines.inventory.item_main)
+                self:processInventory(nested_inventory)
+            end
+        end
     end
 end
 
+---@param record LuaRecord
+function BlueprintMigrator:processRecord(record)
+    if not (record.valid and record.valid_for_write) then return end
+
+    if record.type == 'blueprint' then
+        self:executeMigration(record)
+    elseif record.type == 'blueprint-book' then
+        for _, nested_record in pairs(record.contents) do
+            self:processRecord(nested_record)
+        end
+    end
+end
+
+---------------------------------------------------------------------------
+
 function Migration:migrateBlueprints()
-    for _, blueprint in pairs(game.blueprints) do
-        migrate_blueprint(blueprint)
+    -- migrate game blueprints
+    for _, record in pairs(game.blueprints) do
+        BlueprintMigrator:processRecord(record)
+    end
+
+    -- migrate blueprints players have in their inventory
+    for _, player in pairs(game.players) do
+        local inventory = player.get_main_inventory()
+        BlueprintMigrator:processInventory(inventory)
     end
 end
 
